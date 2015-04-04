bfc2d73c48db0b74170ad50b8679e2ffb9c77211
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-04-04 23:47:09 +0300

    Корректировка лодера для services.yml. Добавление в composer.json клиента redis

---
+++ src/Rottenwood/NexusBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rottenwood\NexusBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('rottenwood_nexus');
+
+        return $treeBuilder;
+    }
+}
--- src/Rottenwood/NexusBundle/RottenwoodNexusBundle.php
+++ src/Rottenwood/NexusBundle/RottenwoodNexusBundle.php
@@ -2,15 +2,8 @@
 
 namespace Rottenwood\NexusBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
 
-class RottenwoodNexusBundle extends Bundle {
-
-    public function build(ContainerBuilder $containerBuilder) {
-        $loader = new Loader\YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-    }
+class RottenwoodNexusBundle extends Bundle
+{
 }
